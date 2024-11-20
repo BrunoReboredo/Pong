5c799ad4a2b73ffc39fcfad3697609d8f064fbb1
BrunoReboredo <brurebo@gmail.com>
2024-11-20 19:07:28 +0100

    descanso

--- Pong/Assets/Scripts/Pelota_controller.cs
+++ Pong/Assets/Scripts/Pelota_controller.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Pelota_controller : MonoBehaviour
 {
     Rigidbody2D rb;
-
+    [SerializeField]float force;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //obtener la referencia del objeto asociado
-        rb.AddForce(new Vector2 (1,1), ForceMode2D.Impulse);
+        rb.AddForce(new Vector2 (1,1) * force, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
