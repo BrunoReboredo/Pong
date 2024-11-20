31666aa6553ad09e276fdf63c0ceb088bbddabce
BrunoReboredo <brurebo@gmail.com>
2024-11-20 18:35:10 +0100

    Ambas palas añadidas con su moviemiento y colisiones en los bordes de las zonas de juego

--- Pong/Assets/Scripts/pala_controller.cs
+++ Pong/Assets/Scripts/pala_controller.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class pala_controller:MonoBehaviour
 {
+
+    //Variables
+    const float Max_Y = 4.1f;
+    const float Min_Y = -4.1f;
+    [SerializeField] float speed = 4.2f; //al ser la misma velocidad, se usa una variable para evitar problemas
     void Start(){
 
     }
     void Update(){
-       if (Input.GetKey("up")) {
+        if(gameObject.CompareTag("pala_dere")){
+                   if (Input.GetKey("up") && transform.position.y <= Max_Y) {
            // transform.Translate(new Vector3(0, 0.1f, 0)); creas el objeto y le das los valores
-           transform.Translate(Vector3.up * 0.1f); //tiene por defecto estos valores de arriba
+           transform.Translate(Vector3.up * speed* Time.deltaTime); //tiene por defecto estos valores de arriba
         }
 
-        if (Input.GetKey("down")) {
+        if (Input.GetKey("down") && transform.position.y >= Min_Y) {
             //transform.Translate(new Vector3(0, -0.1f, 0)); creas el objeto y le das los valores
-            transform.Translate(Vector3.down * 0.1f);
+            transform.Translate(Vector3.down * speed* Time.deltaTime);
         }
+        } else if(gameObject.CompareTag("pala_izqui")){
+       if (Input.GetKey("w") && transform.position.y <= Max_Y) {
+           // transform.Translate(new Vector3(0, 0.1f, 0)); creas el objeto y le das los valores
+           transform.Translate(Vector3.up * speed* Time.deltaTime); //tiene por defecto estos valores de arriba
+        }
+
+        if (Input.GetKey("s") && transform.position.y >= Min_Y) {
+            //transform.Translate(new Vector3(0, -0.1f, 0)); creas el objeto y le das los valores
+            transform.Translate(Vector3.down * speed* Time.deltaTime);
+        }
+        }
+
     }
 }
