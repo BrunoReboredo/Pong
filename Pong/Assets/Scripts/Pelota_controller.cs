3f7ac6988b599c400088a16bbe62fc89884af956
BrunoReboredo <brurebo@gmail.com>
2024-11-21 11:08:51 +0100

    La pelota se reinicia al marcar gol (problema aceleramiento pelota)

--- Pong/Assets/Scripts/Pelota_controller.cs
+++ Pong/Assets/Scripts/Pelota_controller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,15 +6,19 @@ public class Pelota_controller : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField]float force;
-    [SerializeField]const float angle_min= -1;
-    [SerializeField]const float angle_max = 1;
+    [SerializeField] float delay;
+    const float angle_min= -20;
+    const float angle_max = 40;
+    const float max_Y = 2.5f;
+    const float min_Y = 2.5f;
+  
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //obtener la referencia del objeto asociado
         //rb.AddForce(new Vector2 (1,1) * force, ForceMode2D.Impulse); // con esta linea la pelota siempre sale en la misma dirección.
         //hacer que salga aleatorio
         int direccionX = Random.Range(0, 2) == 0 ? -1 : 1;
-        LanzamientoDePelota(direccionX);
+        StartCoroutine(LanzamientoDePelota(direccionX));
     }
 
     // Update is called once per frame
@@ -34,17 +39,34 @@ public class Pelota_controller : MonoBehaviour
 
     //se activa cuando la bola entra en contacto con los muros laterales (se ha marcado gol)
     void OnTriggerEnter2D (Collider2D other){
-        Debug.Log ("GOl "+ other.tag);
+        Debug.Log ("Gol "+ other.tag);
+
+        if (other.tag== "Porteria_dere"){
+            StartCoroutine(LanzamientoDePelota(1));
+        }else if (other.tag == "Porteria_izqui"){
+            StartCoroutine(LanzamientoDePelota(-1));
+        }
     }
 
     //Hace un random para elegir el angulo de lanzamiento de la pelota. Eje X e Y
-    void LanzamientoDePelota(int direccionX){
-        float angulo  = Random.Range(angle_min, angle_max);
+    IEnumerator LanzamientoDePelota(int direccionX){
+
+        yield return new WaitForSeconds(delay);
+
+        // Calculo de la posición vertical de lanzamiento
+        float posY = Random.Range(min_Y, max_Y);
+        transform.position = new Vector3 (0, posY, 0);
+
+        //Calculo del vector de lanzamiento
+        float angulo  = Random.Range(angle_min, angle_max)* Mathf.Deg2Rad;
         float x = Mathf.Cos(angulo) * direccionX;
         // hace q si sale 0 sea -1 (para abajo) y si sale uno sale para arriba
         int direccionY = Random.Range(0, 2) == 0 ? -1 : 1;
         float y = Mathf.Sin(angulo) * direccionY;
         Vector2 impulse = new Vector2(x, y);
+
+        //resetear la velocidad de la pelota cad vez q sale
+       // rb.linearVelocityY = Vector2.zero;
         rb.AddForce(impulse * force, ForceMode2D.Impulse);
     }
 
